e7929516b4dd8be0403cabe190526349059ae3e9
pndminh <miphanng@gmail.com>
2023-05-28 09:25:57 +0700

    product list for admin, still need to add css

---
+++ frontend/src/Modules/Admin/Products/productList.js
@@ -0,0 +1,104 @@
+import React, { Fragment, useContext } from 'react';
+import {getProducts, deleteProduct} from "./fetchAPI";
+import { useNavigate, Link } from "react-router-dom";
+import { ProductContext } from "./index";
+
+function ProductList(props){
+    let history = useNavigate();
+
+    const { data, dispatch } = useContext(ProductContext);
+    const {products} = data;
+    const [loading, setLoading] = useState(false);
+
+    uuseEffect(() => {
+        fetchData();
+    })
+
+    const fetchData = async () => {
+        let resData = await getProducts();
+        setTimeout(() => {
+            if (resData & resData.Products) {
+                dispatch({
+                    type: "fetchProductsChangeState",
+                    payload: resData.Products,
+                });
+            }
+        }, 1000);
+    };
+
+    const deleteProductReq = async (id) => {
+        var deleteFunc = await deleteProduct(id);
+        if (deleteFunc.error){
+            console.log(deleteFunc.error);
+        }else if (deleteFunc.success) {
+            console.log(deleteFunc.success);
+            fetchData();
+        }
+    }
+
+    return (
+        <Fragment>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products && products.length > 0 ? (
+                            products.map((item, key) => {
+                                return (
+                                    <ProductList product={item}
+                                        editProduct={(id, product, type) =>
+                                            editProduct(id, product, type)
+                                        }
+                                        deleteProduct={(id) => deleteProductReq(id)}
+                                        key={key}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <tr>
+                                <td>No product found</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                </div>
+            </Fragment>
+    );
+}
+    const ProductList = ({product, deleteProduct, editProduct}) => {
+        return (
+            <Fragment>
+                <tr>
+                    <td>
+                        {product.name.length > 15 ? product.Description.subString(1,15) + "..."
+                        : product.name}
+                    </td>
+                    <td>
+                        {produc.description.slice(0,15)}...
+                    </td>
+                    <td> //w=h=12px
+                        <img src={`${apiURL}/uploads/products/${product.pImages[0]}`}/>
+                    </td>
+                    <td>{product.price}</td>
+                    <td>{product.category.name}</td>
+                    <td>{product.stock}</td>
+                    <td>
+                        <i class="fa-solid fa-pen-to-square"
+                            onClick={(e) => editProduct(product.id, product, true)}></i>
+                        <i class="fa-solid fa-trash"
+                            onClick={(e) => deleteProduct(product.id)}></i>
+                    </td>
+                </tr>
+            </Fragment>
+        )
+}
+export default ProductList;
